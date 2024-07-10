56abe9291e4d1117818e6caac8573c97ed5ceca1
HoangNguyen <hoangnguyen5301@gmail.com>
2024-07-10 08:09:22 +0700

    commit5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router'
 
 // component
 import { HeaderComponent } from "../components/header/header.component";
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +12,8 @@ import { HeaderComponent } from "../components/header/header.component";
     styleUrl: './app.component.css',
     imports: [
       RouterOutlet,
-      HeaderComponent
+      HeaderComponent,
+      FooterComponent
     ]
 })
 export class AppComponent {
